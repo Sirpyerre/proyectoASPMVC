9455e4f67d99255267e9175e818cdee2cb2f8477
Pedro Rojas <peter@monoforms.com>
2019-01-15 09:20:41 -0600

    Agregar archivos de proyecto.

---
+++ aplicacionMvc/Controllers/Home2Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+
+namespace aplicacionMvc.Controllers
+{
+    public class Home2Controller : Controller
+    {
+        // GET: Home2
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Ver()
+        {
+           
+            return View(Alumno.Obtener());
+        }
+
+        public ActionResult Guardar(Alumno alumno)
+        {
+            return Redirect("~/home/index");
+        }
+    }
+}
